ddea33ac79915020cfb5d836056b177965d61462
Randall Vásquez <ravazquez@gmail.com>
2018-01-17 18:51:20 +0100

    Renamed VL.OpenCVSharp to VL.OpenCV. Added default Mat and cleaned up unnecessary IsAssigned nodes.

---
+++ src/DefaultMat.cs
@@ -0,0 +1,9 @@
+﻿using OpenCvSharp;
+
+namespace VL.OpenCV
+{
+    public static class DefaultMat
+    {
+        public static readonly Mat Damon = new Mat(new Size(1, 1), MatType.CV_8UC3);
+    }
+}
