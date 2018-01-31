33edbfa55bab33a314aeb2fff40784d9ec4a2748
Elias Holzer <elias@vvvv.org>
2018-01-31 02:04:31 +0100

    Added Mat -> Image converters - couldn't commit VL files because of some git line ending shit

---
+++ src/UnsupportedMatTypeException.cs
@@ -0,0 +1,16 @@
+﻿using OpenCvSharp;
+using System;
+
+namespace VL.OpenCV
+{
+    public class UnsupportedMatTypeException : Exception
+    {
+        public MatType Type { get; }
+
+        public UnsupportedMatTypeException(MatType type)
+            : base($"Unsupported mat type {type}")
+        {
+            Type = type;
+        }
+    }
+}
