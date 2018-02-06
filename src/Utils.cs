d702466ae3c52475fb0a1783c82da121b22bc86c
Randall Vásquez <ravazquez@gmail.com>
2018-02-06 22:54:35 +0100

    Added implementations for all Trackers in OpenCV.  Added some basic conversion tools between OpenCV and VL types.  Added FindChessboardCorners node.  Started implementing experimental VideoFile Source node with better performance.

---
+++ src/Utils.cs
@@ -0,0 +1,16 @@
+﻿using OpenCvSharp;
+
+namespace VL.OpenCV
+{
+    public static class Utils
+    {
+        public static bool FindChessboardCorners(
+            InputArray image,
+            Size patternSize,
+            out Point2f[] corners,
+            ChessboardFlags flags = ChessboardFlags.AdaptiveThresh | ChessboardFlags.NormalizeImage)
+        {
+            return Cv2.FindChessboardCorners(image, patternSize, out corners, flags);
+        }
+    }
+}
