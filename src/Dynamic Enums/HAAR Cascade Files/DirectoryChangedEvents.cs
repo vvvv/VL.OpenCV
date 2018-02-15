009e01227c6aa654b46b764ea189dafd20e7cb30
Randall Vásquez <ravazquez@gmail.com>
2018-02-15 23:55:29 +0100

    Added HAAR cascade files to project.  Added entry in nuspec file to include HAAR cascade files.  Added dynamic enum to displayed currently available HAAR cascade files.

---
+++ src/Dynamic Enums/HAAR Cascade Files/DirectoryChangedEvents.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VL.Core;
+using VL.Lib.Reactive;
+using System.Reflection;
+
+namespace VL.OpenCV
+{
+    public static class DirectoryChangedEvents
+    {
+        static FileSystemWatcher fsw = new FileSystemWatcher(Assembly.GetExecutingAssembly().Location + "\\..\\..\\..\\content\\haarcascades");
+        
+        static readonly IObservable<EventPattern<FileSystemEventArgs>> FileCreatedObservable;
+
+        static readonly IObservable<EventPattern<FileSystemEventArgs>> FileDeletedObservable;
+
+        static readonly IObservable<EventPattern<FileSystemEventArgs>> DirectoryChangedObservable;
+
+        /// <summary>
+        /// Checks for device additions and removals, buffers multiple events in 2 second windows. Can fire multiple times
+        /// </summary>
+        public static IObservable<EventPattern<FileSystemEventArgs>> DirectoryChanged => DirectoryChangedObservable;
+
+        static DirectoryChangedEvents()
+        {
+            fsw.EnableRaisingEvents = true;
+
+            FileCreatedObservable = Observable.FromEventPattern<FileSystemEventArgs>(fsw, "Created");
+            FileCreatedObservable.Subscribe();
+
+            FileDeletedObservable = Observable.FromEventPattern<FileSystemEventArgs>(fsw, "Deleted");
+            FileDeletedObservable.Subscribe();
+
+            DirectoryChangedObservable = Observable.Merge(FileCreatedObservable, FileDeletedObservable);
+        }
+    }
+
+}
---
+++ src/Dynamic Enums/HAAR Cascade Files/HAARCascadeFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VL.Lib.Collections;
+
+namespace VL.OpenCV
+{
+
+    [Serializable]
+    public class HAARCascadeFile : DynamicEnumBase<HAARCascadeFile, HAARCascadeFileDefinition>
+    {
+        public HAARCascadeFile(string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static HAARCascadeFile CreateDefault() => CreateDefaultBase("No HAAR cascade files found");
+    }
+}
