5abeabf1cc614d2ccefccb8b3dbec1ed6447dc9c
Elias Holzer <elias@vvvv.org>
2019-09-16 17:06:03 +0200

    Fixed type initialization exception if haarcascade directory is not present in build output directory

---
+++ src/Dynamic Enums/HAAR Cascade Files/HAARCascadeResolver.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace VL.OpenCV
+{
+    static class HAARCascadeResolver
+    {
+        internal static string ResolveHaarcascadesDirectory()
+        {
+            // TODO: Fix nuget package in a way that this is always the same path relative from this assembly!
+            var thisDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            {
+                var insidePackagesFolder = Path.Combine(thisDir, "..", "..", "content", "haarcascades");
+                if (Directory.Exists(insidePackagesFolder))
+                    return insidePackagesFolder;
+            }
+            {
+                var insideBuildOutputFolder = Path.Combine(thisDir, "content", "haarcascades");
+                if (Directory.Exists(insideBuildOutputFolder))
+                    return insideBuildOutputFolder;
+            }
+            return null;
+        }
+    }
+}
